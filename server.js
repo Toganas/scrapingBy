8eec621ee3295a1a736be86bc2bd5d5f41d3827d
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-08-03 11:58:10 -0500

    removed the routes so that they could be put in their own files.  required the routes now that they're in a separate file

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const exphbs = require("express-handlebars");
 
 // intialize express
 const app = express();
-
+var routes = require("./routes")
 // require all models
 const db = require("./models")
 
@@ -23,7 +23,7 @@ app.use(express.static("public"));
 // Parse request as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(routes)
 // setting handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
@@ -35,72 +35,6 @@ const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlin
 mongoose.connect(MONGODB_URI);
 // mongoose.connect("mongodb://loclhost/mongoHeadlines", { useNewUrlParser: true })
 
-// creating  routes
-
-app.get("/", (req,res)=>{
-    db.Article.find({}, (err,data)=>{
-        res.render("index")
-    })
-})
-
-// GET route for scraping
-app.get("/scrape", (req, res) => {
-    axios.get("https://bleacherreport.com/world-football").then((response) => {
-        // make cheerio $
-        const $ = cheerio.load(response.data);
-
-        // grabbing articles
-        $("div.articleContent").each(function (i, element) {
-            const result = {};
-
-            result.title = $(this).children("a").children("h3").text();
-            result.link = $(this).children("a.articleTitle").attr("href")
-            console.log(result);
-
-            // create new Article
-
-            db.Article.create(result).then((dbArticle) => {
-                console.log(dbArticle);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-        });
-        res.send("Scrape Complete")
-    })
-});
-
-// Show all articles
-app.get("/articles", (req,res)=>{
-    db.Article.find({})
-    .then((dbArticle)=>{
-        res.json(dbArticle);
-    })
-    .catch((err)=>{
-        res.json(err);
-    });
-});
-
-// route to grab a specific article by id
-
-app.get("/articles/:id",(req,res)=>{
-    db.Article.findOne({ _id: req.params.id }).populate("note").then((dbArticle)=>{
-        res.json(dbArticle);
-    }).catch((err)=>{
-        res.json(err);
-    });
-});
-
-// Update article's notes by id
-app.post("/articles/:id",(req,res)=>{
-    db.Note.create(req.body).then((dbNote)=>{
-        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-    }).then((dbArticle)=>{
-        res.json(dbArticle);
-    }).catch((err)=>{
-        res.json(err);
-    });
-});
 
 
 app.listen(PORT, () => {
