4ccb27745ace8797603c32380fe667292e566b64
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-08-02 22:15:58 -0500

    created the Article model

---
+++ models/Article.js
@@ -0,0 +1,28 @@
+// require mongoose
+const mongoose = require("mongoose");
+
+// save schema
+const Schema = mongoose.Schema;
+
+// create new Article Schema
+
+const ArticleSchema = newSchema ({
+    title:{
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    note: {
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    }
+});
+
+// Create the model above
+const Article = mongoose.model("Article", ArticleSchema)
+
+// Export module
+module.exports = Article;
