e6198efe52c1af2c5fdd38609e62c0bc09ec892d
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-08-02 22:05:20 -0500

    ES6 changes.  Added in comments.  added in mongoose instead of mongojs.  Created scraping path to scrape from https://bleacherreport.com/world-football

--- server.js
+++ server.js
@@ -1,22 +1,57 @@
-// express and mongojs
+// express and mongoose
 const express = require("express");
-const mongojs = require("mongojs");
-var PORT = process.env.PORT || 3000;
+const mongoose = require("mongoose")
+// port
+const PORT = process.env.PORT || 3000;
 
-// axios and cheerio
+// axios and cheerio - needed for scraping
 const axios = require("axios");
 const cheerio = require("cheerio");
 
 // handlebars
 const exphbs = require("express-handlebars");
 
+// intialize express
 const app = express();
+
+// require all models
+// const db = require("./models")
 
 // set static content as public
 app.use(express.static("public"));
 
-app.use(express.urlencoded({extended: true}));
+// Parse request as JSON
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+// setting handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+// connect to mongodb
+
+mongoose.connect("mongodb://localhost/scrapingby", { useNewUrlParser: true });
+
+// creating scraping routes
+
+// GET route for scraping
+app.get("/scrape", (req, res) => {
+    axios.get("https://bleacherreport.com/world-football").then((response) => {
+        // make cheerio $
+        const $ = cheerio.load(response.data);
+
+        // grabbing articles
+        $("div.articleContent").each(function (i, element) {
+            const result = {};
+
+            result.title = $(this).children("a").children("h3").text();
+            result.link = $(this).children("a.articleTitle").attr("href")
+            // result.link =$(this).attr("href");
+            console.log(result);
+
+        })
+    })
+})
+app.listen(PORT, () => {
+    console.log("running at: http://localhost:" + PORT);
+});
