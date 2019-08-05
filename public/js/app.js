7e6907ccf56cc8a6f4f2b65bc340cf5da84c5863
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-08-05 01:04:08 -0500

    made it so buttons would be added.  also only shows favorites that are set to false.  Shows everything in a card now.  The button makes it so it updates favorite from false to true.

--- public/js/app.js
+++ public/js/app.js
@@ -1,45 +1,73 @@
 // grab json articles
 $.getJSON("/articles", (data) => {
+
     for (var i = 0; i < data.length; i++) {
-        $("#articles").append(`<p data-id="${data[i]._id}">${data[i].title}<br />${data[i].link}<br />${data[i].summary}</p>`);
-    }
+        if (data[i].favorite === false) {
+            // $("#articles").append(`<p data-id="${data[i]._id}">${data[i].title}<br />${data[i].link}<br />${data[i].summary}</p>`);
+            $("#articles").append(`
+        <div class="card mb-3">
+            <div class="card-header" >
+                <h2><a href="${data[i].link}">${data[i].title}</a></h2>
+                <a class = "btn btn-primary save" data-id="${data[i]._id}">Save Article</a>
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">${data[i].summary}<h5>
+            </div>
+        </div>
+                `);
+        };
+    };
 });
 
-$(document).on("click", "p", function(){
+$(document).on("click", "h5", function () {
     $("#notes").empty();
-    const thisId= $(this).attr("data-id");
+    const thisId = $(this).attr("data-id");
 
     $.ajax({
         method: "GET",
         url: "/articles/" + thisId
-    }).then ((data)=>{
+    }).then((data) => {
         console.log(data);
 
         $("#notes").append(`<h2>${data.title}</h2>`);
         $("#notes").append(`<input id="titleinput name="title">`);
         $("#notes").append(`<textarea id="bodyinput" name="body"></textarea>`);
         $("#notes").append(`<button data-id="${data._id}" id="savenote">Save Note</button>`);
-        
-        if(data.note) {
+
+        if (data.note) {
             $("#titleinput").val(data.note.title);
             $("#bodyinput").val(data.note.body);
         };
     });
 });
 
-$(document).on("click", "#savenote", function() {
+$(document).on("click", "#savenote", function () {
     const thisId = $(this).attr("data-id");
 
     $.ajax({
         method: "POST",
-        url: "/articles/" +thisId,
+        url: "/articles/" + thisId,
         data: {
             title: $("#titleinput").val(),
             body: $("#bodyinput").val()
         }
-    }).then((data)=>{
+    }).then((data) => {
         $("#notes").empty();
     });
     $("#titleinput").val("");
     $("#bodyinput").val("");
 });
+
+$(document).on("click", ".save", function () {
+    const thisId = $(this).attr("data-id")
+    console.log(thisId)
+    $.ajax({
+        method: "POST",
+        url: "/favorite/" + thisId,
+        data: {
+            favorite: true
+        }
+    }).then((data) => {
+        location.reload();
+    })
+})
