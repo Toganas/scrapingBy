b89d126ab2bd29e92604f2d29ad9dd75bad256c5
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-08-06 09:15:31 -0500

    removing files

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var router = require("express").Router();
-var articleRoutes = require("./article");
-var scrapeRoutes = require("./scrape");
-
-router.use("./scrape", scrapeRoutes);
-router.use("./article", articleRoutes);
-
-
-module.exports = router
